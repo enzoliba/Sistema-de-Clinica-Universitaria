d69a4ced21428b5b26db26be06ee2a05448a6c92
Enzo Linares Barboza <enzoalonsoliba@gmail.com>
2025-12-28 00:58:59 -0500

    Add files via upload

---
+++ src/clinicauniversitaria/ClinicaUniversitaria.java
@@ -0,0 +1,10 @@
+package clinicauniversitaria;
+
+import GUI.Principal;
+
+public class ClinicaUniversitaria {
+    public static void main(String[] args) {
+        Principal pri = new Principal();
+        pri.setVisible(true);
+    }
+}
